cc73063783b19483a1539acb80e23a5e2c42513b
Flavien DEDOURGE <fdedourge@gmail.com>
2020-08-23 16:23:58 +0200

    feat(Hellforge): Changes app ready call.
    Deletes a test.

--- src/features/hellforge/hellforge.ts
+++ src/features/hellforge/hellforge.ts
@@ -1,27 +1,17 @@
 import _ from "lodash"
 import * as Discord from "discord.js"
-import { token } from "../../../credentials.json"
-import { Ready } from "../ready/ready"
+import { AppReadyServivce } from "../ready/app-ready.service"
 
-export class Hellforge {
-  private static _instance: Hellforge
+class Hellforge {
   private _client = new Discord.Client()
 
-  public static getInstance(): Hellforge {
-    if(_.isNil(Hellforge._instance)) {
-      Hellforge._instance = new Hellforge()
-    }
-    return Hellforge._instance
+  constructor() {
+    this.init()
   }
 
   public init(): void {
-    this._client.on('ready', () => {
-      Ready.getInstance().setActivity(this._client.user, "WATCHING")
-      Ready.getInstance().log(this._client.user?.tag)
-    })
-
-    this._client.login(token)
+    AppReadyServivce.getInstance().init(this._client)
   }
 }
 
-Hellforge.getInstance().init()
+new Hellforge()
