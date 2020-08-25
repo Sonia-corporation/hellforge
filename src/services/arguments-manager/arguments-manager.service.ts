d71dd2f4821f4a04326b618e0c25a3bff4561884
Sonia Dedourge <soniadedourge@gmail.com>
2020-08-25 15:57:31 +0200

    feat(Arguments manager): Creates it.

---
+++ src/services/arguments-manager/arguments-manager.service.ts
@@ -0,0 +1,24 @@
+import _ from "lodash"
+import { Message } from "discord.js"
+import { DisplayMessageService } from "../display-message/display-message.sevice"
+
+export class ArgumentsManagerService {
+  private static _instance: ArgumentsManagerService
+
+  public static getInstance(): ArgumentsManagerService {
+    if (_.isNil(ArgumentsManagerService._instance)) {
+      ArgumentsManagerService._instance = new ArgumentsManagerService()
+    }
+    return ArgumentsManagerService._instance
+  }
+
+  public getArguments(message: Message, botPrefix: string) {
+    return message.content.slice(botPrefix.length).split(/ +/)
+  }
+
+  public extractCommand(messageToSend: Message, entries: string[]) {
+    const usableBotCommand = entries.shift()
+    if (usableBotCommand) return usableBotCommand.toLowerCase()
+    else return DisplayMessageService.getInstance().displayMessage(messageToSend, 'Yes?')
+  }
+}
