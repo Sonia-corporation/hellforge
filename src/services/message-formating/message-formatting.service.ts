6d58a8009390a5ec1c8218e5b68978be04cccaa5
Asmodée Bellanie <asmodee.bellanie@gmail.com>
2020-08-25 21:25:58 +0200

    feat(Message formatting service): Removes the strike.

--- src/services/message-formating/message-formatting.service.ts
+++ src/services/message-formating/message-formatting.service.ts
@@ -14,27 +14,23 @@ export class MessageFormattingService {
     return this[formatToApply](text)
   }
 
-  public bold(text: string) {
+  public bold(text: string): string {
     return `**${text}**`
   }
 
-  public italic(text: string) {
-    return `**${text}**`
+  public italic(text: string): string {
+    return `*${text}*`
   }
 
-  public italicBold(text: string) {
-    return `**${text}**`
+  public italicBold(text: string): string {
+    return `***${text}***`
   }
 
-  public lineThrough(text: string) {
-    return `**${text}**`
+  public lineThrough(text: string): string {
+    return `~~${text}~~`
   }
 
-  public strike(text: string) {
-    return `**${text}**`
-  }
-
-  public underline(text: string) {
-    return `**${text}**`
+  public underline(text: string): string {
+    return `__${text}__`
   }
 }
