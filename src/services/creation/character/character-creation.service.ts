6ed6b93d41ce7b946b6e4085b00675a24b8996f8
Asmodée Bellanie <asmodee.bellanie@gmail.com>
2020-08-25 22:40:34 +0200

    feat(Character creation): Adds step 2.

--- src/services/creation/character/character-creation.service.ts
+++ src/services/creation/character/character-creation.service.ts
@@ -4,7 +4,9 @@ import { TextFormats } from "../../../enums/text-formats.enum"
 import { DisplayMessageService } from "../../display-message/display-message.service"
 import { CharacterService } from "../../character/character.service"
 import { MessageFormattingService } from "../../message-formating/message-formatting.service"
-import { StateManagerService } from '../../state-manager/state-manager.service'
+import { StateManagerService } from "../../state-manager/state-manager.service"
+import { StateNamesEnum } from "../../../enums/state-names.enum"
+import { StatNamesEnum } from 'src/enums/stat-names.enum'
 
 export class CharacterCreationService {
   private static _instance: CharacterCreationService
@@ -16,14 +18,14 @@ export class CharacterCreationService {
     return CharacterCreationService._instance
   }
 
-  public init(message: Message) {
+  public init(message: Message): void {
     CharacterService.getInstance().getEntity(message.author.id)
     .then((characterFound) => {
       if (!characterFound) {
         const currentState = StateManagerService.getInstance().getBotState()
 
         if (currentState.state === 'Normal') {
-          StateManagerService.getInstance().setBotState('CharacterCreation', 1, `${message.author.id},`) // The data from the state will have a CSV format value.
+          StateManagerService.getInstance().setBotState(message.author.id, StateNamesEnum.CHARACTER_CREATION, 1, `${message.author.id},`) // The data from the state will have a CSV format value.
           DisplayMessageService.getInstance().message(message, `Welcome to the character creation. Type in the name of your character below. You can type 'exit' to quit this mode, or 'save' to end the process but still continue it later.`)
         }
       }
@@ -34,10 +36,17 @@ export class CharacterCreationService {
     })
   }
 
-  public setCharacterName(message: Message) {
-    const newData = StateManagerService.getInstance().getBotState().data + `${message.content},`
-    StateManagerService.getInstance().setBotState('Normal', 0, newData) // TODO: Create step 2.
+  public setCharacterName(message: Message): void {
+    const newData = `${StateManagerService.getInstance().getBotState().data}${message.content},`
+    StateManagerService.getInstance().setBotState(message.author.id, StateNamesEnum.CHARACTER_CREATION, 2, newData)
     const formattedCharacterName = MessageFormattingService.getInstance().format(TextFormats.BOLD, message.content)
     DisplayMessageService.getInstance().message(message, `Pleased to meet ${formattedCharacterName}... What will be their foremost stat?`)
+  }
+
+  public setCharacterFirstBonus(message: Message): void {
+    const newData = `${StateManagerService.getInstance().getBotState().data}${message.content},`
+    StateManagerService.getInstance().setBotState(message.author.id, StateNamesEnum.CHARACTER_CREATION, 3, newData)
+    const formattedCharacterFisrtStatBonus = MessageFormattingService.getInstance().format(TextFormats.ITALIC_BOLD, message.content)
+    DisplayMessageService.getInstance().message(message, `So, they will be proficient at ${formattedCharacterFisrtStatBonus}, I hope they will have a good use of it.`)
   }
 }
