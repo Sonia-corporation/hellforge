14dc2268a20a9764ff177965a98224bba4917efb
Sonia Dedourge <soniadedourge@gmail.com>
2020-08-26 17:04:11 +0200

    feat(Colors): Creates two enums.

---
+++ src/enums/colors.enum.ts
@@ -0,0 +1,6 @@
+export enum ColorsEnum {
+  RED_PIGMENT = "#EA2027",
+  DEEP_COVE = "#130f40",
+  PURPLE_APPLE = "#6ab04c",
+  FIERY_FUCHSIA = "#B33771"
+}
---
+++ src/enums/embed-colors.enum.ts
@@ -0,0 +1,8 @@
+import { ColorsEnum } from './colors.enum';
+
+export enum EmbedColorsEnum {
+  SUCCESS = ColorsEnum.PURPLE_APPLE,
+  INFO = ColorsEnum.DEEP_COVE,
+  HELP = ColorsEnum.FIERY_FUCHSIA,
+  ERROR = ColorsEnum.RED_PIGMENT
+}
