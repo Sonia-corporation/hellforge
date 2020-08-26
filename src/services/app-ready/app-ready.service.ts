329ad0e61de5ec85b18551d9ee4a91de5df88fec
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-26 16:16:20 +0200

    refactor(app): spellchecking and configuration fix and refactor
    rbnrb eslint rules are bad by the way

--- src/services/app-ready/app-ready.service.ts
+++ src/services/app-ready/app-ready.service.ts
@@ -1,33 +1,33 @@
-import _ from "lodash"
-import { Client } from "discord.js"
-import { token } from "../../../credentials.json"
-import { ActivityService } from "../activity/activity.service"
-import { MongooseConnectService } from "../../services/mongoose-connect/mongoose-connect.service"
+import { Client } from 'discord.js';
+import _ from 'lodash';
+import { token } from '../../../credentials.json';
+import { MongooseConnectService } from '../../services/mongoose-connect/mongoose-connect.service';
+import { ActivityService } from '../activity/activity.service';
 
-export class AppReadyServivce {
-  private static _instance: AppReadyServivce
+export class AppReadyService {
+  private static _instance: AppReadyService;
 
-  public static getInstance(): AppReadyServivce {
-    if (_.isNil(AppReadyServivce._instance)) {
-      AppReadyServivce._instance = new AppReadyServivce()
+  public static getInstance(): AppReadyService {
+    if (_.isNil(AppReadyService._instance)) {
+      AppReadyService._instance = new AppReadyService();
     }
-    return AppReadyServivce._instance
+    return AppReadyService._instance;
   }
 
   public init(client: Client): void {
-    MongooseConnectService.getInstance().init()
+    MongooseConnectService.getInstance().init();
 
-    client.on("ready", () => {
+    client.on('ready', () => {
       if (client.user) {
-        ActivityService.getInstance().setActivity(client.user, "WATCHING")
-        this._log(client.user.tag)
+        ActivityService.getInstance().setActivity(client.user, 'WATCHING');
+        this._log(client.user.tag);
       }
-    })
+    });
 
-    client.login(token)
+    client.login(token);
   }
 
   private _log(usertag: string): void {
-    return console.log(`Logged in as ${usertag}!`)
+    return console.log(`Logged in as ${usertag}!`);
   }
 }
