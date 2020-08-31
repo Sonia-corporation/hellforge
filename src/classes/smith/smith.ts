78c3f3a57ab493d52391541f49db352ec26d8e5f
Sonia Dedourge <soniadedourge@gmail.com>
2020-08-31 15:07:49 +0200

    feat(Smith): Moves the smith functions.

--- src/classes/smith/smith.ts
+++ src/classes/smith/smith.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { IBehaviour } from "../../types/character/behaviour";
 import { IEquipment } from "../../types/shop/equipment";
 import { IItem } from "../../types/shop/item";
@@ -5,6 +6,13 @@ import { ISmith } from "../../types/character/smith";
 import { IStat } from "../../types/character/stat";
 import { IWeapon } from "../../types/shop/weapon";
 import { SmithTypesEnum } from "../../enums/smith-types.enum";
+import { EquipmentSpotsEnum } from "../../enums/equipment-spots.enum";
+import { ObjectTypesEnum } from "../../enums/object-types.enum";
+import { StatNamesEnum } from "../../enums/stat-names.enum";
+import { SmithBehaviourEnum } from "../../enums/smith-behaviour";
+import { WeaponCategoriesEnum } from "../../enums/weapon-categories.enum";
+import Smiths from "../../data/models/smith-schema";
+import names from "../../data/texts/smiths/names.json";
 
 export class Smith implements ISmith {
   public behaviour: IBehaviour;
@@ -17,21 +25,99 @@ export class Smith implements ISmith {
   public type: SmithTypesEnum;
   public weapons?: IWeapon[];
 
-  constructor(smith: ISmith) {
-    this.behaviour = smith.behaviour;
-    this.description = smith.description;
-    if (smith.equipment) {
-      this.equipment = smith.equipment;
+  constructor() {
+    this.behaviour = this._generateBehaviour();
+    this.description = ``;
+    this.name = this._generateName();
+    this.startingLevel = this._generateStartingLevel(10);
+    this.stats = this._generateStats();
+    this.type = this._generateType();
+    this.weapons = this._generateWeapons(_.random(1, 3));
+  }
+
+  public async generateEntity(): Promise<ISmith> {
+    const smith: ISmith = {
+      behaviour: this.behaviour,
+      description: this.description,
+      name: this.name,
+      startingLevel: this.startingLevel,
+      stats: this.stats,
+      type: this.type,
+      weapons: this.weapons,
+    };
+
+    void Smiths.create(smith);
+
+    return Promise.resolve(smith);
+  }
+
+  private _generateBehaviour(): IBehaviour {
+    const behaviour: IBehaviour = {
+      description: ``,
+      name: _.sample(SmithBehaviourEnum) || SmithBehaviourEnum.SHY,
+    };
+
+    return behaviour;
+  }
+
+  private _generateName(): string {
+    const name = _.sample(names.names);
+
+    if (name) {
+      return name;
     }
-    this.name = smith.name;
-    if (smith.items) {
-      this.items = smith.items;
+    return `Bob`;
+  }
+
+  private _generateStartingLevel(max: number): number {
+    return _.random(1, max);
+  }
+
+  private _generateStats(): IStat[] {
+    const generatedStats: IStat[] = [];
+
+    _.forEach(StatNamesEnum, (statName: StatNamesEnum): void => {
+      if (statName !== StatNamesEnum.LUCK) {
+        generatedStats.push({
+          description: ``,
+          name: statName,
+          value: _.random(4, 11),
+        });
+      } else {
+        generatedStats.push({
+          description: ``,
+          name: StatNamesEnum.LUCK,
+          value: _.random(0, 5),
+        });
+      }
+    });
+
+    return generatedStats;
+  }
+
+  private _generateType(): SmithTypesEnum {
+    const type = _.sample(SmithTypesEnum);
+
+    if (type) {
+      return type;
     }
-    this.startingLevel = smith.startingLevel;
-    this.stats = smith.stats;
-    this.type = smith.type;
-    if (smith.weapons) {
-      this.weapons = smith.weapons;
+
+    return SmithTypesEnum.BLACKSMITH;
+  }
+
+  private _generateWeapons(weaponsNumber: number): IWeapon[] | undefined {
+    const weapons: IWeapon[] = [];
+
+    for (let i = 1; i <= weaponsNumber; i++) {
+      weapons.push({
+        category: WeaponCategoriesEnum.BOW,
+        description: ``,
+        name: `Uber Weapon`,
+        spot: EquipmentSpotsEnum.PRIMARY_HAND,
+        type: ObjectTypesEnum.EQUIPMENT,
+      });
     }
+
+    return weapons;
   }
 }
--- src/features/commands/smith/smith-command.service.ts
+++ src/features/commands/smith/smith-command.service.ts
@@ -1,17 +1,6 @@
 import _ from "lodash";
 import { Smith } from "../../../classes/smith/smith";
-import names from "../../../data/texts/smiths/names.json";
 import { ISmith } from "../../../types/character/smith";
-import { IStat } from "../../../types/character/stat";
-import { IWeapon } from "../../../types/shop/weapon";
-import { IBehaviour } from "../../../types/character/behaviour";
-import { SmithTypesEnum } from "../../../enums/smith-types.enum";
-import { StatNamesEnum } from "../../../enums/stat-names.enum";
-import { SmithBehaviourEnum } from "../../../enums/smith-behaviour";
-import { ObjectTypesEnum } from "../../../enums/object-types.enum";
-import { EquipmentSpotsEnum } from "../../../enums/equipment-spots.enum";
-import { WeaponCategoriesEnum } from "../../../enums/weapon-categories.enum";
-import Smiths from "../../../data/models/smith-schema";
 
 export class SmithCommandService {
   private static _instance: SmithCommandService;
@@ -24,92 +13,6 @@ export class SmithCommandService {
   }
 
   public async generate(): Promise<ISmith> {
-    const behaviour = this._generateBehaviour();
-    const description = ``;
-    const name = this._generateName();
-    const startingLevel = _.random(1, 10);
-    const stats = this._generateStats();
-    const type = this._generateType();
-    const weapons = this._generateWeapons(_.random(1, 3));
-
-    const smith: ISmith = new Smith({
-      behaviour,
-      description,
-      name,
-      startingLevel,
-      stats,
-      type,
-      weapons,
-    });
-
-    void Smiths.create(smith);
-
-    return Promise.resolve(smith);
-  }
-
-  private _generateBehaviour(): IBehaviour {
-    const behaviour: IBehaviour = {
-      description: ``,
-      name: _.sample(SmithBehaviourEnum) || SmithBehaviourEnum.SHY,
-    };
-
-    return behaviour;
-  }
-
-  private _generateName(): string {
-    const name = _.sample(names);
-
-    if (name) {
-      return name[0];
-    }
-    return `Bob`;
-  }
-
-  private _generateStats(): IStat[] {
-    const generatedStats: IStat[] = [];
-
-    _.forEach(StatNamesEnum, (statName: StatNamesEnum): void => {
-      if (statName !== StatNamesEnum.LUCK) {
-        generatedStats.push({
-          description: ``,
-          name: statName,
-          value: _.random(4, 11),
-        });
-      } else {
-        generatedStats.push({
-          description: ``,
-          name: StatNamesEnum.LUCK,
-          value: _.random(0, 5),
-        });
-      }
-    });
-
-    return generatedStats;
-  }
-
-  private _generateType(): SmithTypesEnum {
-    const type = _.sample(SmithTypesEnum);
-
-    if (type) {
-      return type;
-    }
-
-    return SmithTypesEnum.BLACKSMITH;
-  }
-
-  private _generateWeapons(weaponsNumber: number): IWeapon[] | undefined {
-    const weapons: IWeapon[] = [];
-
-    for (let i = 1; i <= weaponsNumber; i++) {
-      weapons.push({
-        category: WeaponCategoriesEnum.BOW,
-        description: ``,
-        name: `Uber Weapon`,
-        spot: EquipmentSpotsEnum.PRIMARY_HAND,
-        type: ObjectTypesEnum.EQUIPMENT,
-      });
-    }
-
-    return weapons;
+    return new Smith().generateEntity();
   }
 }
