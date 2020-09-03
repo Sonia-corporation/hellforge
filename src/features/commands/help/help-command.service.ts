f8f2760ec1fd4532d8880c70d8be967b4f052f46
Asmodée Bellanie <asmodee.bellanie@gmail.com>
2020-09-03 14:12:08 +0200

    feat(Help): Adds a canvas.

--- src/features/commands/help/help-command.service.ts
+++ src/features/commands/help/help-command.service.ts
@@ -1,5 +1,6 @@
 import _ from "lodash";
-import { Message, MessageEmbed } from "discord.js";
+import { Message, MessageEmbed, MessageAttachment } from "discord.js";
+import Canvas from "canvas";
 import { HelpsEnum } from "../../../enums/helps.enum";
 import { EmbedColorsEnum } from "../../../enums/embed-colors.enum";
 import { DisplayMessageService } from "../../../services/display-message/display-message.service";
@@ -14,7 +15,10 @@ export class HelpCommandService {
     return HelpCommandService._instance;
   }
 
-  public message(message: Message, helpToDisplay: HelpsEnum): Promise<Message> {
+  public async message(
+    message: Message,
+    helpToDisplay: HelpsEnum
+  ): Promise<Message> {
     const embed = new MessageEmbed({
       color: EmbedColorsEnum.HELP,
     });
@@ -22,6 +26,19 @@ export class HelpCommandService {
       embed.title = `Welcome to Hell`;
       embed.description = `Your goal, as an inhabitant of the nether realm, is to create the most famous forge of all Hell: The Hellforge. In order to become that, here is what you can do to help you in your journey.`;
     }
-    return DisplayMessageService.getInstance().message(message, embed);
+
+    const canvas = Canvas.createCanvas(700, 300);
+    const context = canvas.getContext(`2d`);
+
+    context.fillStyle = `#B33771`;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const helpAttachment = new MessageAttachment(canvas.toBuffer(), `help.png`);
+
+    return DisplayMessageService.getInstance().message(
+      message,
+      embed,
+      helpAttachment
+    );
   }
 }
